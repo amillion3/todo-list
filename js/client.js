ee8a139d1637eb3568a1f9643813e3ce9c8d2b23
Andy Million <amillion3@gmail.com>
2017-05-20 14:57:31 -0500

    Messed with checkbox and labels a bunch
    Very little functional change, but a checkbox now appears.

--- js/client.js
+++ js/client.js
@@ -12,14 +12,10 @@ var todoList = {
   deleteTodo: function(position) {
     this.todos.splice(position, 1);
   },
-
-
   toggleCompleted: function(position) {
     var todo = this.todos[position];
     todo.completed = !todo.completed;
   },
-
-
   toggleAll: function() {
     var totalTodos = this.todos.length;
     var completedTodos = 0;
@@ -91,7 +87,8 @@ var view = {
       todoLi.id = position;
       todoLi.textContent = todoTextWithCompletion;
       todoLi.appendChild(this.createDeleteButton());
-      todoLi.appendChild(this.createCheckButton());
+      todoLi.appendChild(this.createCheckButton(position));
+      todoLi.appendChild(this.createCheckButtonLabel(position));
       todosUl.appendChild(todoLi);
     }, this);
   },
@@ -102,13 +99,18 @@ var view = {
     deleteButton.className = 'deleteButton btn btn-sm btn-danger';
     return deleteButton;
   },
-  createCheckButton: function() {
+  createCheckButton: function(position) {  //checkbox
     var todoLiCheckBox = document.createElement('input');
     todoLiCheckBox.type = 'checkbox';
-    todoLiCheckBox.className = 'css-checkbox';
-    //need a label? it's putting it in the DOM now at least...
-    //need a position?
+    todoLiCheckBox.className = '';
+    todoLiCheckBox.name = 'checkbox' + position;  // .name equals checkbox plus the position of the 'to do'
     return todoLiCheckBox;
+  },
+  createCheckButtonLabel: function(position) {  //label for the checkbox
+    var todoLiCheckBoxLabel = document.createElement('label');
+    todoLiCheckBoxLabel.htmlFor = 'checkbox' + position;  // .name equals checkbox plus the position of the 'to do'
+    todoLiCheckBoxLabel.className = '';
+    return todoLiCheckBoxLabel;
   },
   setupEventListeners: function() {
     var todosUl = document.querySelector('ul');
